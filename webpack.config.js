b315afbaf4f7b81a5a90f1f03b8291491f85feb6
Bogdan Vlasenkov <vlasenkov174@mail.ru>
2021-06-20 14:03:33 +0500

    lesson 1

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: './src/index.tsx',
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+}
