fbd775d0391db25e8676a30d75b115f1a5b5d032
Bogdan Vlasenkov <vlasenkov174@mail.ru>
2021-07-14 23:25:54 +0500

    1 lesson правки 14.07.21

--- src/lesson1/mathOperators.ts
+++ src/lesson1/mathOperators.ts
@@ -1,21 +1,21 @@
 export type MathOperationType = (first: number, second: number) => number;
 
 // записываем функции, которые будут в нашем калькуляторе
-export const sum : MathOperationType = ( (first, second) => {
-    return first + second
-} )
+export const sum: MathOperationType = (first, second) => {
+  return first + second;
+};
 
-export const minus : MathOperationType = ( (first, second) => {
-    return first - second
-} )
+export const minus: MathOperationType = (first, second) => {
+  return first - second;
+};
 
-export const mul : MathOperationType = ( (first, second) => {
-    return first * second
-} )
+export const mul: MathOperationType = (first, second) => {
+  return first * second;
+};
 
-export const div : MathOperationType = ( (first, second) => {
-    return first / second
-} )
+export const div: MathOperationType = (first, second) => {
+  return first / second;
+};
 
 export const mathPriorities: number[] = [1, 2];
 
@@ -24,17 +24,17 @@ const [FIRST, SECOND] = mathPriorities;
 // Экспортируем математические операторы
 
 export const mathOperators: { [key: string]: MathOperationType } = {
-    "*": mul,
-    "/": div,
-    "+": sum,
-    "-": minus,
-}
+  "*": mul,
+  "/": div,
+  "+": sum,
+  "-": minus,
+};
 
 // Пишем приоритет для операций
 
 export const mathOperatorsPriorities: { [key: string]: number } = {
-    "*": FIRST,
-    "/": FIRST,
-    "+": SECOND,
-    "-": SECOND,
-}
+  "*": FIRST,
+  "/": FIRST,
+  "+": SECOND,
+  "-": SECOND,
+};
--- src/lesson1/runner.test.ts
+++ src/lesson1/runner.test.ts
@@ -1,15 +1,12 @@
 import { runner } from "./runner";
 
-describe("Сложные выражения", () => {
-    it("4 + 5 * 9", () => {
-        expect(runner("4 + 5 * 9")).toEqual(49);
-    });
-
-    it("36 - 12 / 4", () => {
-        expect(runner("36 - 12 / 4")).toEqual(33);
-    });
-
-    it("5 * 5 * 5", () => {
-        expect(runner("5 * 5 * 5")).toEqual(125);
-    });
+test.each([
+  { expression: "4 + 5 * 9", expected: 49 },
+  { expression: "36 - 12 / 4", expected: 33 },
+  { expression: "5 * 5 * 5", expected: 125 },
+  { expression: "1 + 3 + 7 * 5", expected: 39 },
+  { expression: "81 / 9 * 9", expected: 81 },
+  { expression: "0 * 2 * 7", expected: 0 },
+])("runner test", ({ expression, expected }) => {
+  expect(runner(expression)).toStrictEqual(expected);
 });
