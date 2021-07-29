bbf8dccae161336335f7f5fb8e80863f2c6c2b08
Bogdan Vlasenkov <vlasenkov174@mail.ru>
2021-07-30 00:32:38 +0500

    1 lesson правки 14.07.21

--- src/lesson1/engine.test.ts
+++ src/lesson1/engine.test.ts
@@ -1,12 +1,12 @@
 import { firstPrioritiesCalc, secondPrioritiesCalc } from "./engine";
 
 describe("Первый приоритет", () => {
-    it("[6 / 2]", () => {
-        expect(firstPrioritiesCalc([6, "/", 2])).toEqual([3]);
+    it("[3 + 6 / 2]", () => {
+        expect(firstPrioritiesCalc([6, "/", 2, "+", 3])).toEqual([3, "+", 3]);
     })
 })
 describe("Второй приоритет", () => {
-    it("[3 + 2]", () => {
-        expect(secondPrioritiesCalc([3, "+", 2])).toEqual(5);
+    it("[3 + 6 + 3]", () => {
+        expect(secondPrioritiesCalc([3, "+", 6, "+", 3])).toEqual(12);
     })
 })
